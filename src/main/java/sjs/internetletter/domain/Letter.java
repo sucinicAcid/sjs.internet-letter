11435a7c068ee70f1f2f290be6d8e92ed8c55a93
sucinicacid <sucinicacid2002@gmail.com>
2021-09-21 22:04:35 +0900

    Feat: "Create Letter class"

---
+++ src/main/java/sjs/internetletter/domain/Letter.java
@@ -0,0 +1,22 @@
+package sjs.internetletter.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Letter {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "LETTER_ID")
+    private Long id;
+
+    private String title;
+    private String content;
+}
