e76f05b708826b32a8acde63ff39d41a047fe1f2
sucinicacid <sucinicacid2002@gmail.com>
2021-09-21 21:39:51 +0900

    Initialize(project)

---
+++ src/main/java/sjs/internetletter/InternetLetterApplication.java
@@ -0,0 +1,13 @@
+package sjs.internetletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetLetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetLetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sjs/internetletter/InternetLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package sjs.internetletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetLetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
