c54706974b1af8cc0bd9719c1ab8e42b25ab5204
sucinicacid <sucinicacid2002@gmail.com>
2021-09-22 17:57:30 +0900

    Feat: "Create LetterService"

---
+++ src/main/java/sjs/internetletter/service/LetterService.java
@@ -0,0 +1,27 @@
+package sjs.internetletter.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sjs.internetletter.domain.Letter;
+import sjs.internetletter.repository.LetterRepository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class LetterService {
+
+    private LetterRepository letterRepository;
+
+    @Transactional
+    public Long join(Letter letter) {
+        return letterRepository.save(letter);
+    }
+
+    public List<Letter> findLetters() {
+        return letterRepository.findAll();
+    }
+}
