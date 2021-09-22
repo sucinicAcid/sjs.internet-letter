e5b69a608f3e673c8899e79d83b4ec5805a2a4a9
sucinicacid <sucinicacid2002@gmail.com>
2021-09-22 21:19:32 +0900

    Feat: "Add POST method that add letter to database"

--- src/main/java/sjs/internetletter/web/letter/LetterController.java
+++ src/main/java/sjs/internetletter/web/letter/LetterController.java
@@ -2,9 +2,13 @@ package sjs.internetletter.web.letter;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sjs.internetletter.domain.letter.Letter;
 import sjs.internetletter.domain.letter.LetterService;
 import sjs.internetletter.web.letter.form.LetterForm;
 
@@ -18,5 +22,18 @@ public class LetterController {
     @GetMapping
     public String addForm(@ModelAttribute("form") LetterForm form) {
         return "letters/addForm";
+    }
+
+    @PostMapping
+    public String addLetter(@Validated @ModelAttribute("form") LetterForm form,
+                            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/letters/addForm";
+        }
+
+        Letter letter = Letter.createLetter(form.getName(), form.getTitle(), form.getContent());
+        letterService.join(letter);
+
+        return "redirect:/letters/complete";
     }
 }
