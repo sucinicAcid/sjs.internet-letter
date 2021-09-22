d09eb5e1616cbeec733edceb6e4d378229a8da9d
sucinicacid <sucinicacid2002@gmail.com>
2021-09-22 19:10:57 +0900

    Feat: "Add GET letterform method"

---
+++ src/main/java/sjs/internetletter/web/letter/LetterController.java
@@ -0,0 +1,22 @@
+package sjs.internetletter.web.letter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sjs.internetletter.domain.letter.LetterService;
+import sjs.internetletter.web.letter.form.LetterForm;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/letters")
+public class LetterController {
+
+    private final LetterService letterService;
+
+    @GetMapping
+    public String addForm(@ModelAttribute LetterForm form) {
+        return "letters/addForm";
+    }
+}
