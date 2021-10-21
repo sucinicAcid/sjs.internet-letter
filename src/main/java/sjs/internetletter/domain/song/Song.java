d4e59667fa7f5c51d89463cb4ed8f915d0cbb8a5
sucinicacid <sucinicacid2002@gmail.com>
2021-10-21 22:19:55 +0900

    Fix: "Change timestamp annotation"

--- src/main/java/sjs/internetletter/domain/song/Song.java
+++ src/main/java/sjs/internetletter/domain/song/Song.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -20,7 +21,7 @@ public class Song {
     private String title;
     private String groupName;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
     private Date addDate;
 
     public static Song createSong(String title, String groupName) {
