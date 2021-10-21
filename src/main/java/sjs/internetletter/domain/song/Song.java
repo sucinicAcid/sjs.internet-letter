3bf36a217ee799a522878c3c46248ffdb6d17909
sucinicacid <sucinicacid2002@gmail.com>
2021-10-21 22:05:28 +0900

    Feat: "Add Song entity"

---
+++ src/main/java/sjs/internetletter/domain/song/Song.java
@@ -0,0 +1,32 @@
+package sjs.internetletter.domain.song;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Song {
+
+    @Id @GeneratedValue
+    @Column(name = "SONG_ID")
+    private Long id;
+
+    private String title;
+    private String groupName;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date addDate;
+
+    public static Song createSong(String title, String groupName) {
+        Song song = new Song();
+        song.setTitle(title);
+        song.setGroupName(groupName);
+        return song;
+    }
+}
